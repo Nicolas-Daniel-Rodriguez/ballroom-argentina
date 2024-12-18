de7279177989d1df2bacdb5524f0c89bc766ff9e
Nicolas D. Rodriguez <ndr126@abc.gob.ar>
2024-12-18 15:39:17 -0300

    instructores

--- src/components/Instructores.js
+++ src/components/Instructores.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useInView } from "react-intersection-observer";
+import Cris from "./../assets/Cristian.png";
+import Mari from "./../assets/marina.png";
 
 const Instructores = () => {
   // Hook para detectar cuando el componente es visible
@@ -13,7 +15,7 @@ const Instructores = () => {
     {
       nombre: "Cristian Javier Rodríguez",
       especialidad: "Campeón de Ballroom",
-      imagen: "https://via.placeholder.com/300x300", // Reemplaza con la URL de tu imagen
+      imagen: Cris, 
       redes: {
         facebook: "#",
         twitter: "#",
@@ -23,7 +25,7 @@ const Instructores = () => {
     {
       nombre: "Marina Gonzalez",
       especialidad: "Campeona de Ballroom",
-      imagen: "https://via.placeholder.com/300x300", // Reemplaza con la URL de tu imagen
+      imagen: Mari, 
       redes: {
         facebook: "#",
         twitter: "#",
@@ -39,56 +41,59 @@ const Instructores = () => {
         <div className="text-center mb-12">
           <h2 className="text-primary text-2xl font-bold">CONOCE A NUESTROS INSTRUCTORES</h2>
           <h1 className="text-4xl md:text-5xl font-bold text-white mt-2">Expertos apasionados en la danza</h1>
-          <p className="text-primary text-xl text-justify mt-4">
+          <p className="text-primary text-xl text-justify mt-4 mb-4 max-w-2xl mx-auto">
             Presentamos a nuestros talentosos y experimentados instructores de danza, 
             dedicados a ayudar a los estudiantes a alcanzar sus metas.
           </p>
         </div>
 
         {/* Contenedor de los instructores */}
-        <div
-          ref={ref}
-          className={`grid grid-cols-1 md:grid-cols-2 gap-8 ${
-            inView ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
-          } transition-all duration-1000`}
-        >
-          {instructores.map((instructor, index) => (
-            <div
-              key={index}
-              className="bg-gray-800 rounded-lg shadow-lg overflow-hidden"
-            >
-              <img
-                src={instructor.imagen}
-                alt={instructor.nombre}
-                className="w-full h-64 object-cover"
-              />
-              <div className="p-6 text-center">
-                <h3 className="text-xl font-semibold text-primary">{instructor.nombre}</h3>
-                <p className="text-gray-400 mt-2">{instructor.especialidad}</p>
-                <div className="flex justify-center mt-4 space-x-4">
-                  <a
-                    href={instructor.redes.facebook}
-                    className="text-gray-400 hover:text-primary"
-                  >
-                    <i className="fab fa-facebook-f"></i>
-                  </a>
-                  <a
-                    href={instructor.redes.twitter}
-                    className="text-gray-400 hover:text-primary"
-                  >
-                    <i className="fab fa-twitter"></i>
-                  </a>
-                  <a
-                    href={instructor.redes.youtube}
-                    className="text-gray-400 hover:text-primary"
-                  >
-                    <i className="fab fa-youtube"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          ))}
+<div
+  ref={ref}
+  className={`flex flex-wrap justify-around gap-10 ${
+    inView ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
+  } transition-all duration-1000`}
+>
+  {instructores.map((instructor, index) => (
+    <div
+      key={index}
+      className="bg-gray-800 rounded-lg shadow-lg overflow-hidden shadow-white transform transition-transform duration-300 hover:scale-110"
+    >
+      <div className="overflow-hidden">
+        <img
+          src={instructor.imagen}
+          alt={instructor.nombre}
+          className="w-full h-64 object-cover mx-auto "
+        />
+      </div>
+      <div className="p-6 text-center">
+        <h3 className="text-xl font-semibold text-primary">{instructor.nombre}</h3>
+        <p className="text-gray-400 mt-2">{instructor.especialidad}</p>
+        <div className="flex justify-center mt-4 space-x-4">
+          <a
+            href={instructor.redes.facebook}
+            className="text-gray-400 hover:text-primary"
+          >
+            <i className="fab fa-facebook-f"></i>
+          </a>
+          <a
+            href={instructor.redes.twitter}
+            className="text-gray-400 hover:text-primary"
+          >
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a
+            href={instructor.redes.youtube}
+            className="text-gray-400 hover:text-primary"
+          >
+            <i className="fab fa-youtube"></i>
+          </a>
         </div>
+      </div>
+    </div>
+  ))}
+</div>
+
 
         {/* Botón "Ver más" 
         <div className="text-center mt-12">
